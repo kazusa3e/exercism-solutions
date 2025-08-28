9eafcffc67f1493fde550432b6f8c01fc211d340
kazusa <i@kazusa.me>
2025-08-28 21:50:09 +0800

    Log Levels

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -1,15 +1,27 @@
+#include <algorithm>
+#include <cstddef>
+#include <sstream>
 #include <string>
 
 namespace log_line {
 std::string message(std::string line) {
-    // return the message
+    const auto pos = line.find(' ');
+    line.erase(0, pos + 1);
+    return line;
 }
 
 std::string log_level(std::string line) {
-    // return the log level
+    const auto e = line.find("]") + line.begin();
+    std::copy_backward(line.begin() + 1, e, e - 1);
+    line.erase(e - line.begin() - 1);
+    return line;
 }
 
 std::string reformat(std::string line) {
-    // return the reformatted message
+    const auto level = log_level(line);
+    const auto msg = message(line);
+    std::ostringstream oss;
+    oss << msg << " (" << level << ")";
+    return oss.str();
 }
 }  // namespace log_line
