15f7c94443550e527ee4b80c92bda51032d43e3c
kazusa <i@kazusa.me>
2025-09-08 07:35:38 +0800

    Power of Troy

--- cpp/power-of-troy/power_of_troy.cpp
+++ cpp/power-of-troy/power_of_troy.cpp
@@ -1,3 +1,25 @@
 #include "power_of_troy.h"
 
-namespace troy {}  // namespace troy
+namespace troy {
+
+void give_new_artifact(human &h, const std::string &p) {
+    h.possession.reset(new artifact {p});
+}
+
+void exchange_artifacts(std::unique_ptr<artifact> &lhs, std::unique_ptr<artifact> &rhs) {
+    std::swap(lhs, rhs);
+}
+
+void manifest_power(human &h, const std::string &p) {
+    h.own_power.reset(new power {p});
+}
+
+void use_power(const human &from, human &to) {
+    to.influenced_by = from.own_power;
+}
+
+int power_intensity(const human &h) {
+    return h.own_power.use_count();
+}
+
+}  // namespace troy
--- cpp/power-of-troy/power_of_troy.h
+++ cpp/power-of-troy/power_of_troy.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
 namespace troy {
 
@@ -15,5 +16,20 @@ struct power {
     power(std::string effect) : effect(effect) {}
     std::string effect;
 };
+
+class human {
+public:
+
+public:
+    std::unique_ptr<artifact> possession;
+    std::shared_ptr<power> own_power;
+    std::shared_ptr<power> influenced_by;
+};
+
+void give_new_artifact(human &h, const std::string &p);
+void exchange_artifacts(std::unique_ptr<artifact> &lhs, std::unique_ptr<artifact> &rhs);
+void manifest_power(human &h, const std::string &p);
+void use_power(const human &from, human &to);
+int power_intensity(const human &h);
 
 }  // namespace troy
