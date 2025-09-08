6a0a83032b5ba555a29ab5c8194c84dbc6322c9a
kazusa <i@kazusa.me>
2025-09-08 08:26:30 +0800

    Speedywagon Foundation

--- cpp/speedywagon/speedywagon.cpp
+++ cpp/speedywagon/speedywagon.cpp
@@ -1,11 +1,33 @@
 #include "speedywagon.h"
+#include <numeric>
 
 namespace speedywagon {
 
 // Enter your code below:
+bool connection_check(const pillar_men_sensor *sensor) {
+    return sensor != nullptr;
+}
+
+unsigned activity_counter(pillar_men_sensor *sensors, unsigned sensors_len) {
+    return std::accumulate(sensors, sensors + sensors_len, 0u,
+                           [](const int acc, const pillar_men_sensor &curr) {
+                               if (!connection_check(&curr)) return acc;
+                               return acc + curr.activity;
+                           });
+}
+
+bool alarm_control(const pillar_men_sensor *sensor) {
+    if (!connection_check(sensor)) return false;
+    return sensor->activity > 0;
+}
+
+bool uv_alarm(pillar_men_sensor *sensor) {
+    if (!connection_check(sensor)) return false;
+    return (uv_light_heuristic(&(sensor->data)) > sensor->activity);
+}
 
 // Please don't change the interface of the uv_light_heuristic function
-int uv_light_heuristic(std::vector<int>* data_array) {
+int uv_light_heuristic(std::vector<int> *data_array) {
     double avg{};
     for (auto element : *data_array) {
         avg += element;
--- cpp/speedywagon/speedywagon.h
+++ cpp/speedywagon/speedywagon.h
@@ -12,5 +12,9 @@ struct pillar_men_sensor {
 };
 
 int uv_light_heuristic(std::vector<int>* data_array);
+bool connection_check(const pillar_men_sensor *sensor);
+unsigned activity_counter(pillar_men_sensor *sensors, unsigned sensors_len);
+bool alarm_control(const pillar_men_sensor *sensor);
+bool uv_alarm(pillar_men_sensor *sensor);
 
 }  // namespace speedywagon
