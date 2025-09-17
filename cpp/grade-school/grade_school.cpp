9e91110a96f0432951f8c0fbc8b4f8c92cc87a41
kazusa <i@kazusa.me>
2025-09-17 13:52:41 +0800

    Grade School

--- cpp/grade-school/grade_school.cpp
+++ cpp/grade-school/grade_school.cpp
@@ -2,6 +2,28 @@
 
 namespace grade_school {
 
-// TODO: add your solution here
+struct school::school_impl_ {
+    school::roster_t roster;
+};
+
+school::school() : impl_(std::make_unique<school_impl_>()) {}
+school::~school() = default;
+
+const school::roster_t &school::roster() const {
+    return impl_->roster;
+}
+
+void school::add(const std::string &name, unsigned grade) {
+    auto &bucket = impl_->roster[grade];
+    const auto pos = std::lower_bound(bucket.begin(), bucket.end(), name);
+    bucket.insert(pos, name);
+}
+
+std::vector<std::string> school::grade(unsigned grade) const {
+    if (const auto pos = impl_->roster.find(grade); pos != impl_->roster.cend()) {
+        return pos->second;
+    }
+    return {};
+}
 
 }  // namespace grade_school
--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include <map>
+#include <memory>
+#include <vector>
+#include <string>
+
 namespace grade_school {
 
-// TODO: add your solution here
+class school {
+public:
+    using roster_t = std::map<int, std::vector<std::string>>;
+
+public:
+    school();
+    ~school();
+    const roster_t &roster() const;
+    void add(const std::string &name, unsigned grade);
+    std::vector<std::string> grade(unsigned) const;
+
+private:
+    struct school_impl_;
+    std::unique_ptr<school_impl_> impl_;
+};
 
 }  // namespace grade_school
