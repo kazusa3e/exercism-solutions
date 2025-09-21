0388f0703e56f9b7e6c8a6499fdb3346a3968cab
kazusa <i@kazusa.me>
2025-09-21 14:49:34 +0800

    Secret Handshake skeleton

---
+++ cpp/secret-handshake/secret_handshake.cpp
@@ -0,0 +1,7 @@
+#include "secret_handshake.h"
+
+namespace secret_handshake {
+
+// TODO: add your solution here
+
+}  // namespace secret_handshake
---
+++ cpp/secret-handshake/secret_handshake.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace secret_handshake {
+
+// TODO: add your solution here
+
+}  // namespace secret_handshake
