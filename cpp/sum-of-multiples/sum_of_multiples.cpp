fa2152911f53b071444ed71b0656cc5239357e0f
kazusa <i@kazusa.me>
2025-10-27 05:32:29 +0000

    Sum of Multiples

--- cpp/sum-of-multiples/sum_of_multiples.cpp
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -1,7 +1,19 @@
 #include "sum_of_multiples.h"
+#include <numeric>
+#include <set>
 
 namespace sum_of_multiples {
 
-// TODO: add your solution here
+auto to(const std::vector<unsigned> &items, unsigned level) -> std::size_t {
+    if (level == 0) return 0;
+    std::set<std::size_t> scores;
+    for (const auto &itm : items) {
+        if (itm == 0) continue;
+        for (unsigned ix = 0; ix < level; ix += itm) {
+            scores.insert(ix);
+        }
+    }
+    return std::accumulate(scores.cbegin(), scores.cend(), 0);
+}
 
 }  // namespace sum_of_multiples
--- cpp/sum-of-multiples/sum_of_multiples.h
+++ cpp/sum-of-multiples/sum_of_multiples.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <vector>
+
 namespace sum_of_multiples {
 
-// TODO: add your solution here
+auto to(const std::vector<unsigned> &items, unsigned level) -> std::size_t;
 
 }  // namespace sum_of_multiples
